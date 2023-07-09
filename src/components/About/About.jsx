f6e8fb6421c6b7d7ae58223f6de739b027739b84
warismee <50206850+warismee@users.noreply.github.com>
2023-07-09 13:16:58 +1200

    fix ipad responsive

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
       <div className='about_content'>
         <div className='about_container'>
           <article className='about_card'>
-            <p>I am graduating in Graduate Diploma in IT at Wellington Institute of Technology and also taking an online course about Web Development on Udemy/YouTube during my free time. I have a strong interest in Front-end development, enjoy coding and enjoy building a beautiful and functional website interface. I am looking for opportunities as a junior developer role at present with the desire to develop my skillset and move toward becoming a full-stack developer in the future.</p>
+            <p>I obtained Graduate Diploma in IT at Wellington Institute of Technology in 2022nu and I am taking an online course about Web Development on Udemy/YouTube during my free time. I have a strong interest in Front-end development, enjoy coding and enjoy building a beautiful and functional website interface. I am looking for opportunities as a junior developer role at present with the desire to develop my skillset and move toward becoming a full-stack developer in the future.</p>
           </article>
         </div>
         <div className='about_container'>
