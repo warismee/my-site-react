b20aa9f7fb2da2ef5203215197fa1b4d0e2787c1
warismee <50206850+warismee@users.noreply.github.com>
2024-05-11 17:05:24 +0700

    updated about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,6 +4,7 @@ import {IoLanguageOutline} from 'react-icons/io5'
 import {MdOutlineTravelExplore} from 'react-icons/md'
 import {SiFuturelearn} from 'react-icons/si'
 import {MdOutlineDirectionsRun} from 'react-icons/md'
+import { GiMountainClimbing } from "react-icons/gi"
 
 const About = () => {
   return (
@@ -12,7 +13,7 @@ const About = () => {
       <div className='about_content'>
         <div className='about_container'>
           <article className='about_card'>
-            <p>I obtained Graduate Diploma in IT at Wellington Institute of Technology in 2022nu and I am taking an online course about Web Development on Udemy/YouTube during my free time. I have a strong interest in Front-end development, enjoy coding and enjoy building a beautiful and functional website interface. I am looking for opportunities as a junior developer role at present with the desire to develop my skillset and move toward becoming a full-stack developer in the future.</p>
+            <p>I obtained Graduate Diploma in IT at Wellington Institute of Technology in 2022 and I am taking an online course about Web Development on Udemy/YouTube during my free time. I have a strong interest in Front-end development, enjoy coding and enjoy building a beautiful and functional website interface. I am looking for opportunities as a junior developer role at present with the desire to develop my skillset and move toward becoming a full-stack developer in the future.</p>
           </article>
         </div>
         <div className='about_container'>
@@ -20,7 +21,7 @@ const About = () => {
             <h4>Personal Interest</h4>
             <h5>Learning languages: Japanese, Mandarin etc.</h5>
             <h5>Joining meet-up: Language Exchange, Software Development</h5>
-            <h5>Traveling, hiking, jogging: Enjoy both leisure and active activities. Love to see nature</h5>
+            <h5>Traveling, hiking, jogging, bouldering: Enjoy both leisure and active activities. Love to see nature</h5>
             <h5>Learning new skills from YouTube and playing games are two main online activities. Usually learn languages and software Development.</h5>
           </article>
         </div>
@@ -35,8 +36,8 @@ const About = () => {
               <h5>Traveling/Hiking</h5>
             </article>
             <article className='about_interest'>
-              <MdOutlineDirectionsRun className='interest_icon'/>
-              <h5>Jogging</h5>
+              <GiMountainClimbing className='interest_icon'/>
+              <h5>Bouldering/Climbing</h5>
             </article>
             <article className='about_interest'>
               <SiFuturelearn className='interest_icon'/>
